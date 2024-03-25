092bca876c7cad6d3deb2b51e5952d53d47b9c5b
Andrei Ungureanu <andrei.reanu@gmail.com>
2024-03-25 22:33:09 +0200

    Add Token , NFT contracts

---
+++ test/project3/scripts/Tokens.ts
@@ -0,0 +1,49 @@
+import { viem } from "hardhat";
+import { parseEther, formatEther } from "viem";
+
+async function main() {
+    const publicClient = await viem.getPublicClient();
+    const [deployer, account1, account2] = await viem.getWalletClients();
+    
+    const tokenContract = await viem.deployContract("MyToken");
+    console.log(`Contract deployed at ${tokenContract.address}`);
+
+    // Fetching the role code
+    const code = await tokenContract.read.MINTER_ROLE();
+
+    // Giving role
+    const roleTx = await tokenContract.write.grantRole([
+    code,
+    account2.account.address,
+    ]);
+    await publicClient.waitForTransactionReceipt({ hash: roleTx });
+
+    const mintTx = await tokenContract.write.mint(
+        [deployer.account.address, parseEther("10")],
+        { account: account2.account }
+    );
+    await publicClient.waitForTransactionReceipt({ hash: mintTx });
+
+    const totalSupply = await tokenContract.read.totalSupply();
+    console.log({ totalSupply });
+
+    // Sending a transaction
+    const tx = await tokenContract.write.transfer([
+        account1.account.address,
+        parseEther("2"),
+    ]);
+    await publicClient.waitForTransactionReceipt({ hash: tx });
+
+    const myBalance = await tokenContract.read.balanceOf([deployer.account.address]);
+    console.log(`My Balance is ${formatEther(myBalance)} ETH`);
+    const otherBalance = await tokenContract.read.balanceOf([account1.account.address]);
+    console.log(
+    `The Balance of Acc1 is ${formatEther(otherBalance)} ETH`
+    );
+
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exitCode = 1;
+});

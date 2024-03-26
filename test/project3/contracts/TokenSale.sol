57261fae1965f93739eb7aee71029b92a95c061d
Andrei Ungureanu <andrei.reanu@gmail.com>
2024-03-26 22:38:40 +0200

    Begin work on TokenSale

---
+++ test/project3/contracts/TokenSale.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.24;
+import {MyToken} from "./MyToken.sol";
+import {MyNFT} from "./MyNFT.sol";
+
+
+contract TokenSale {
+    uint256 public ratio;
+    uint256 public price;
+    MyToken public paymentToken;
+    MyNFT public nftCollection;
+
+    constructor(uint256 _ratio, uint256 _price, MyToken _paymentToken, MyNFT _nftCollection) {
+        ratio = _ratio;
+        price = _price;
+        paymentToken = _paymentToken;
+        nftCollection = _nftCollection;
+    }
+}

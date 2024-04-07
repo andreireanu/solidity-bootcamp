a1973e24f5acca259bc657c77e5d1f900ccecafa
Andrei Ungureanu <andrei.reanu@gmail.com>
2024-04-07 21:39:23 +0300

    Added coupling FE <-> BE

--- test/project-name/src/app.service.ts
+++ test/project-name/src/app.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@nestjs/common';
-import { createPublicClient, http, formatEther } from 'viem';
+import { createPublicClient, createWalletClient,  http, formatEther } from 'viem';
 import * as chains from "viem/chains";
-import * as tokenJson from '../assets/MyToken.json'
- 
+import * as tokenJson from './assets/MyToken.json'
+import * as process from 'process'; 
+
 @Injectable()
 export class AppService {
     publicClient;
+    walletClient; 
   
     constructor() {
       this.publicClient = createPublicClient({
-        chain: chains.sepolia,  
+        chain: chains. sepolia,  
         transport: http(process.env.RPC_ENDPOINT_URL),
+      }); 
+    
+      this.walletClient = createWalletClient({
+        chain: chains. sepolia,  
+        transport: http(process.env.RPC_ENDPOINT_URL),
+        key: process.env.PRIVATE_KEY, 
       }); 
     }
 
@@ -19,7 +27,7 @@ export class AppService {
   } 
 
   getContractAddress(): string {
-    return "0x020615D5F5C4A12ac04EFB19c558f767cD1Bba94";
+    return process.env.TOKEN_ADDRESS;
   }  
  
   async getTokenName(): Promise<any> {
@@ -39,11 +47,34 @@ export class AppService {
     })) as bigint;
     return formatEther(totalSupply);
   }
-  getTokenBalance(address: string) {
+   
+  async getTokenBalance(address: string) {
+    const tokenBalance = (await this.publicClient.readContract({
+      address: this.getContractAddress() as `0x{string}`,
+      abi: tokenJson.abi,
+      functionName: "balanceOf",
+      args: [address ]
+    })) as bigint; 
+    return formatEther(tokenBalance);
+  }
+  async getTransactionReceipt(hash: string) {
+    const transactionReceipt = await this.publicClient.getTransactionReceipt({hash}); 
+    transactionReceipt.blockNumber = transactionReceipt.blockNumber.toString();
+    transactionReceipt.gasUsed = transactionReceipt.gasUsed.toString();
+    transactionReceipt.cumulativeGasUsed = transactionReceipt.cumulativeGasUsed.toString();
+    transactionReceipt.effectiveGasPrice = transactionReceipt.effectiveGasPrice.toString();
+    return transactionReceipt; 
+  } 
+
+  getServerWalletAddress() {
+    return this.walletClient.account.address; 
+
+  }
+  checkMinterRole(address: string) {
     throw new Error('Method not implemented.');
   }
-  getTransactionReceipt(hash: string) {
-    throw new Error('Method not implemented.');
+  mintTokens(address: any) {
+    return `Minting tokens to ${address}`; 
   } 
 
 }
--- test/project-name/src/main.ts
+++ test/project-name/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import 'dotenv/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('API example')
@@ -14,6 +16,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
